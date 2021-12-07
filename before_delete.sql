7d147bc785ced83f02193fdd8f097b1a39a3c5e4
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-12-07 18:20:06 +0200

    Added position BEFORE DELETE triger

--- before_delete.sql
+++ before_delete.sql
@@ -1,12 +1,29 @@
+DELIMITER $$
+USE `polnyi_7_41`$$
+
 --------------------------------
--- pharmacy
+-- street
 --------------------------------
 DROP TRIGGER IF EXISTS `polnyi_7_41`.`street_BEFORE_DELETE`;
 
-DELIMITER $$
-USE `polnyi_7_41`$$
-CREATE DEFINER = CURRENT_USER TRIGGER `polnyi_7_41`.`street_BEFORE_DELETE` BEFORE DELETE ON `street` FOR EACH ROW
+CREATE DEFINER=`root`@`localhost` TRIGGER `pharmacy_BEFORE_UPDATE` BEFORE UPDATE ON `pharmacy` FOR EACH ROW 
 BEGIN
 	signal sqlstate "45000" set message_text = 'Data from table "street" cannot be deleted';
 END$$
+
+
+--------------------------------
+-- position
+--------------------------------
+DROP TRIGGER IF EXISTS `polnyi_7_41`.`position_BEFORE_DELETE`;
+
+CREATE DEFINER = CURRENT_USER TRIGGER `polnyi_7_41`.`position_BEFORE_DELETE` BEFORE DELETE ON `position` FOR EACH ROW
+BEGIN
+	declare is_position_present bool;
+    select exists(select * from worker where worker.`position` = old.`name`) into is_position_present;
+    if is_position_present
+		then signal sqlstate "45000" set message_text = 'Cannot delete position if workers with this position present';
+	end if;
+END$$
+
 DELIMITER ;
