2b9db7c39bf744c3b275fa1351fe102ca4ada52b
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-12-07 17:14:17 +0200

    Added cure_has_targed BEFORE INSERT script

--- before_insert.sql
+++ before_insert.sql
@@ -20,7 +20,6 @@ END$$
 ----------------
 DROP TRIGGER IF EXISTS `polnyi_7_41`.`worker_BEFORE_INSERT`;
 
-USE `polnyi_7_41`$$
 CREATE DEFINER = CURRENT_USER TRIGGER `polnyi_7_41`.`worker_BEFORE_INSERT` BEFORE INSERT ON `worker` FOR EACH ROW
 BEGIN
     declare position_name varchar(50);
@@ -32,6 +31,24 @@ BEGIN
     select `id` into pharmacy_id from pharmacy where pharmacy.id = new.`pharmacy_id`;
     if (pharmacy_id is null)
         then signal sqlstate '45000' set message_text = 'Cannot insert new value: wrond "pharmacy_id" parameter provided';
+    end if;
+END$$
+
+----------------
+-- cure_has_target
+----------------
+DROP TRIGGER IF EXISTS `polnyi_7_41`.`cure_has_target_BEFORE_INSERT`;
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `cure_has_target_BEFORE_INSERT` BEFORE INSERT ON `cure_has_target` FOR EACH ROW BEGIN
+    declare cure_id int;
+    declare c_target varchar(50);
+    select `id` into cure_id from cure where cure.`id` = new.`cure_id`;
+    if (cure_id is null)
+        then signal sqlstate "45000" set message_text = 'Cannot insert new value: wrong "cure_id" parameter provided';
+    end if;
+    select `target` into c_target from cure_target where cure_target.`target` = new.`target`;
+    if (c_target is null)
+        then signal sqlstate "45000" set message_text = 'Cannot insert new value: wrong "target" parameter provided';
     end if;
 END$$
 
