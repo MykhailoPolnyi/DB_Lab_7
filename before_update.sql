e7f3333a0abfd1aeb615f2bb28307296c62ae790
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-12-07 17:47:55 +0200

    Added worker BEFORE UPDATE trigers

--- before_update.sql
+++ before_update.sql
@@ -15,6 +15,27 @@ BEGIN
     end if;
 END$$
 
+
+--------------------------------
+-- worker
+--------------------------------
+DROP TRIGGER IF EXISTS `polnyi_7_41`.`worker_BEFORE_UPDATE`;
+
+CREATE DEFINER = CURRENT_USER TRIGGER `polnyi_7_41`.`worker_BEFORE_UPDATE` BEFORE UPDATE ON `worker` FOR EACH ROW
+BEGIN
+	declare position_name varchar(50);
+	declare pharmacy_id int;
+    select `name` into position_name from position where position.`name` = new.`position`;
+    if (position_name is null)
+		then signal sqlstate '45000' set message_text = 'Cannot insert new value: wrong "position" parameter provided';
+	end if;
+	select `id` into pharmacy_id from pharmacy where pharmacy.id = new.`pharmacy_id`;
+    if (pharmacy_id is null)
+		then signal sqlstate '45000' set message_text = 'Cannot insert new value: wrond "pharmacy_id" parameter provided';
+	end if;
+END$$
+
+
 --------------------------------
 -- cure_has_target
 --------------------------------
